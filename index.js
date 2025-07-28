b84956ffff065d833400733765b790579a84d740
imadityasingh07 <adityasingh20637@gmail.com>
2025-07-28 14:50:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+// What is a Data Structure JavaScript
+// Information Technology is all about 
+// Storing data 
+// Doing operation in data 
+// Transferring data 
+// Practical example:Ordering a phone
+// hamne ek phone order kiya amazon or flipkart se Aab isha order ke andar kai sare data Generate kar rahe hai.ex:hamari Information: Order Information.ye Amazon ya Flipkart ke server par rakhi gayi hai.Data ke rakhne ke tarike ko Data Structure kahte hai
+// Data should be stored in a way so it is easier to manage and use
+// As a Software Engineer hane apne Data Structure ko aise rakhna hai ki future me hame jab kabhi use karna ho to dikkat na ho ham easy way me use kar le
+// Data Structure are ways to organize and manage data
+// Few real world examples of different types of data
+// Concept of casting
+// What is Casting:jab murtikar ek murati banata hai to wo ek structure ka Ishatemal karta hai ye ek khali jagah hia jaha par murtikar molted/liquid metal andar rakh deta hai.yaha par molted metak ko ham data kah sakte hai.jab isko thanda hone diya to ye jo liquid metal hai thanda hone ke baad shape ya structure ka Aakar le lega aur ek khubshurat si murati bahar nikal kar mil jayegi yaha par ek achha  structure laga aur ek acha molted metal laga dono chize jaruri hai similar ek achha data hona ex:Ordering ,My Information this is all a data ishaka hone jaruri hai but ek aur chiz jaruri hai ek ham data ko kis 
+// structure me rakh rahe hai. agar ye structure galat hua to ye murati galat ho jayegi ye itani khubsurat nahi rahegi aise hi computer science me structure sahi nahi raha to hamara jo program wo itana efficient nahi hoga itana achha nahi hoga aur na hi accha work karega jitna usko karna chahiye aur structure ka sochana aur kaun da structure ka ishatemal karna.ye jo concept hote hai data structur ke agonastic hote hai ye har progrmming language par use hote hai specialy used in backend developer ke liye jyada use hote hai.
+// Example 1 - ToDo list 
+// .To Do list items
+// Exercise
+// Practice programming
+// Complete coding assignment
+// Reading Novels 
+// . All items have equal importance
+// . Any item can be taken and done first
+// I will keep ToDo items in an Array
+// sabse jyada use hone wala Data Structure hai Array
+// Example 2- Selling tickets online
+// Limited tickets - 100 tickets
+// 1000s of people want to buy these tickets
+// First come First Server
+// I will keep users in a queue data structure
+// Example 3- Family members and relationship
+// There is hierarchy
+// There are relationships
+// I will use Tree data structure to keep this data
+// Example 4 - Friends on Facebook
+// There are people 
+// People have relationships(Friendship) with other people 
+// There is no hierarchy
+// I will use Graph data Structure 
+// Why should I learn Data Structures
+// Efficient code : Software programs where data is involved become more efficient if you use correct data structure
+// Improved coding skills
+// Essential for coding interviews
+// Better understanding of algorithms
+// What is Algorithms
+// Algorithms wah logic hai jo usha data par kaam karte hai aur kuch kuch specific problem ko solve karte hai
+// My Next Concept 
+// I will cover all major data structures
+// I will cover them using practical examples
+// I will use JavaScript to implement these data Structure 
